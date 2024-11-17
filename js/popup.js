314f87dca50023e5160f066e22e5600684504e29
Victoria Tomova <victoriatomova55@gmail.com>
2024-11-17 18:53:19 +0200

    Sunfood-cover

---
+++ js/popup.js
@@ -0,0 +1,4 @@
+function togglePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
+}
